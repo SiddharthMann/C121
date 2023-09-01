9e37be7168a67b93d0de4e75171d3761a0f99172
SiddharthMann <58979636+SiddharthMann@users.noreply.github.com>
2023-09-01 18:53:52 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,46 @@
+import cv2
+import time
+import numpy as np
+
+fourCC = cv2.VideoWriter_fourcc(*'XVID')
+output_file= cv2.VideoWriter('output.avi', fourCC, 20.0, (640, 480))
+capture=cv2.VideoCapture(0)
+time.sleep(3)
+
+bg= 0
+
+for i in range(60):
+    ret, bg= capture.read()
+
+bg= np.flip(bg, axis=1)
+
+while(capture.isOpened()):
+    ret, img= capture.read()
+    if not ret:
+        break
+
+    img = np.flip(img, axis=1)
+    hsv= cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_red=np.array([0,120,50])
+    upper_red=np.array([10, 255, 255])
+    mask1= cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red= np.array([170, 120, 70])
+    upper_red= np.array([180, 255, 255])
+    mask2= cv2.inRange(hsv, lower_red, upper_red)
+
+    mask1=mask1+mask2
+
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3,3), np.uint8))
+    mask2 = cv2.bitwise_not(mask1)
+    result1= cv2.bitwise_and(img, img, mask=mask2)
+    result2= cv2.bitwise_and(bg, bg, mask=mask1)
+    finalOutput= cv2.addWeighted(result1, 1, result2, 1, 0)
+
+    output_file.write(finalOutput)
+    cv2.imshow('magic', finalOutput)
+    cv2.waitKey(1)
+
+capture.release()
+cv2.destroyAllWindows()
